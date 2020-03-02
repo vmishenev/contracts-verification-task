36ccbacb14dbc3651ce90f6c65cf3c64ad824a82
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-03-02 09:52:38 +0300

    Add tree model

---
+++ src/main/kotlin/org/jetbrains/dummy/lang/tree/DummyLangVisitor.kt
@@ -0,0 +1,53 @@
+package org.jetbrains.dummy.lang.tree
+
+abstract class DummyLangVisitor<out R, in D> {
+    abstract fun visitElement(element: Element, data: D): R
+
+    open fun visitFile(file: File, data: D): R {
+        return visitElement(file, data)
+    }
+
+    open fun visitFunctionDeclaration(functionDeclaration: FunctionDeclaration, data: D): R {
+        return visitElement(functionDeclaration, data)
+    }
+
+    open fun visitBlock(block: Block, data: D): R {
+        return visitElement(block, data)
+    }
+
+    open fun visitStatement(statement: Statement, data: D): R {
+        return visitElement(statement, data)
+    }
+
+    open fun visitAssignment(assignment: Assignment, data: D): R {
+        return visitStatement(assignment, data)
+    }
+
+    open fun visitIfStatement(ifStatement: IfStatement, data: D): R {
+        return visitStatement(ifStatement, data)
+    }
+
+    open fun visitVariableDeclaration(variableDeclaration: VariableDeclaration, data: D): R {
+        return visitStatement(variableDeclaration, data)
+    }
+
+    open fun visitExpression(expression: Expression, data: D): R {
+        return visitStatement(expression, data)
+    }
+
+    open fun visitVariableAccess(variableAccess: VariableAccess, data: D): R {
+        return visitExpression(variableAccess, data)
+    }
+
+    open fun visitIntConst(intConst: IntConst, data: D): R {
+        return visitExpression(intConst, data)
+    }
+
+    open fun visitBooleanConst(booleanConst: BooleanConst, data: D): R {
+        return visitExpression(booleanConst, data)
+    }
+
+    open fun visitFunctionCall(functionCall: FunctionCall, data: D): R {
+        return visitExpression(functionCall, data)
+    }
+}
---
+++ src/main/kotlin/org/jetbrains/dummy/lang/tree/Tree.kt
@@ -0,0 +1,150 @@
+package org.jetbrains.dummy.lang.tree
+
+sealed class Element {
+    abstract val line: Int
+
+    abstract fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R
+    abstract fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D)
+}
+
+class File(
+    override val line: Int,
+    val functions: List<FunctionDeclaration>
+) : Element() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitFile(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {
+        functions.forEach { it.accept(visitor, data) }
+    }
+}
+
+class FunctionDeclaration(
+    override val line: Int,
+    val name: String,
+    val parameters: List<String>,
+    val body: Block
+) : Element() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitFunctionDeclaration(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {
+        body.accept(visitor, data)
+    }
+}
+
+class Block(
+    override val line: Int,
+    val statements: List<Statement>
+) : Element() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitBlock(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {
+        statements.forEach { it.accept(visitor, data) }
+    }
+}
+
+// --------------- Statements ---------------
+
+sealed class Statement : Element()
+
+// variable = rhs
+class Assignment(
+    override val line: Int,
+    val variable: String,
+    val rhs: Expression
+) : Statement() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitAssignment(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {
+        rhs.accept(visitor, data)
+    }
+}
+
+class IfStatement(
+    override val line: Int,
+    val condition: Expression,
+    val thenBlock: Block,
+    val elseBlock: Block?
+) : Statement() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitIfStatement(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {
+        condition.accept(visitor, data)
+        thenBlock.accept(visitor, data)
+        elseBlock?.accept(visitor, data)
+    }
+}
+
+class VariableDeclaration(
+    override val line: Int,
+    val name: String,
+    val initializer: Expression?
+) : Statement() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitVariableDeclaration(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {
+        initializer?.accept(visitor, data)
+    }
+}
+
+// --------------- Expressions ---------------
+
+sealed class Expression : Statement()
+
+class VariableAccess(
+    override val line: Int,
+    val name: String
+) : Expression() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitVariableAccess(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {}
+}
+
+class IntConst(
+    override val line: Int,
+    val value: Int
+) : Expression() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitIntConst(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {}
+}
+
+class BooleanConst(
+    override val line: Int,
+    val value: Boolean
+) : Expression() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitBooleanConst(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {}
+}
+
+class FunctionCall(
+    override val line: Int,
+    val function: String,
+    val arguments: List<Expression>
+) : Expression() {
+    override fun <R, D> accept(visitor: DummyLangVisitor<R, D>, data: D): R {
+        return visitor.visitFunctionCall(this, data)
+    }
+
+    override fun <D> acceptChildren(visitor: DummyLangVisitor<*, D>, data: D) {
+        arguments.forEach { it.accept(visitor, data) }
+    }
+}
