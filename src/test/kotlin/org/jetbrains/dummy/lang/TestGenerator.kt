59a9ca185a9d5cba7cea6a6beeede1001c632ce8
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-03-18 13:00:06 +0300

    Add docs for test generator

--- src/test/kotlin/org/jetbrains/dummy/lang/TestGenerator.kt
+++ src/test/kotlin/org/jetbrains/dummy/lang/TestGenerator.kt
@@ -4,7 +4,31 @@ import org.jetbrains.dummy.lang.utils.prettyPrinter
 import java.io.File
 
 fun main() {
-    val testDataDir = File("testData")
+    generateTests(
+        testDataRoot = "testData",
+        abstractTestClassName = "AbstractDummyLanguageTest"
+    )
+
+//    Пример того, как сгенерировать тесты для файлов в директории `testData/someOtherTests`
+//    generateTests(
+//        testDataRoot = "testData/someOtherTests",
+//        abstractTestClassName = "AbstractDummyLanguageTest"
+//    )
+}
+
+/**
+ * [testDataRoot] -- относительный путь к директории, в которой хранятся тестовые файлы
+ * [abstractTestClassName] -- имя абстрактного тестового класса, на основе которого будут сгенерированны тесты
+ *     Требования к [abstractTestClassName]:
+ *     - имя класса должно начинаться с перфикса `Abstract`
+ *     - класс должен находиться в пакете `org.jetbrains.dummy.lang`
+ *     - класс должен содержать в себе метод `fun doTest(path: String)`
+ *
+ * Генератор создаёт тесты только для тестовых файлов, которые находятся непосредственно в
+ *     переданной директории, и не ходит по вложенным директориям
+ */
+private fun generateTests(testDataRoot: String, abstractTestClassName: String) {
+    val testDataDir = File(testDataRoot)
     val parentPrefix = testDataDir.absolutePath + File.separator
     val fileNames = testDataDir.listFiles()!!
         .filter { it.extension == "dummy" }
@@ -13,8 +37,9 @@ fun main() {
         }.sorted().toList()
 
     val commonPrefix = parentPrefix.removePrefix(File(".").absolutePath.dropLast(1))
+    val generatedTestName = abstractTestClassName.replace("Abstract", "") + "Generated"
 
-    File("src/test/kotlin/org/jetbrains/dummy/lang/DummyLanguageTestGenerated.kt").prettyPrinter().use { printer ->
+    File("src/test/kotlin/org/jetbrains/dummy/lang/$generatedTestName.kt").prettyPrinter().use { printer ->
         with(printer) {
             println("package org.jetbrains.dummy.lang")
             println()
@@ -28,7 +53,7 @@ fun main() {
             }
             println()
 
-            println("class DummyLanguageTestGenerated : AbstractDummyLanguageTest() {")
+            println("class $generatedTestName : $abstractTestClassName() {")
             withIndent {
                 fileNames.forEachIndexed { index, fileName ->
                     println("@Test")
